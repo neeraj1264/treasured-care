64ffa5b49444d4f7b49b7ac9d9fa55fde47e879a
NeerajQA <neeraj1264qa@gmail.com>
2025-05-08 23:16:03 +0530

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
-          <Link 
+          <Link  
             to="/" 
             className="text-black text-2xl font-bold tracking-tight hover:text-blue-200 transition-colors duration-200"
           >
