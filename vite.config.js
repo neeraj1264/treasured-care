0587c6f0e633a9e1710280a0a59483f1df3f6ddc
NeerajQA <neeraj1264qa@gmail.com>
2025-05-09 11:57:06 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'
 export default defineConfig({
+   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 10000,
+    port: parseInt(process.env.PORT) || 10000,
   },
   preview: {
+     plugins: [react()],
     host: '0.0.0.0',
-    port: 10000,
+    port: parseInt(process.env.PORT) || 10000,
     allowedHosts: ['.onrender.com'], // Allow all Render subdomains
   },
 });
